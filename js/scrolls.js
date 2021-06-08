c4dd5aa7ddaadc39a953ea3299713ceee2a68c51
Nikita Smolyaninov <nikita.smolaninov@gmail.com>
2021-06-08 15:46:13 +0300

    Added normal header

--- js/scrolls.js
+++ js/scrolls.js
@@ -14,3 +14,19 @@ window.onscroll = function changeNavbarHeight()
 	}
 	prevHeight = document.documentElement.scrollTop;
 }
+
+// parallax
+
+var parallaxElements = document.getElementsByClassName("parallax");
+
+var parallaxElementsNumber = parallaxElements.length;
+
+document.addEventListener("scroll", function(e) { DoParallax(e); });
+
+function DoParallax(){
+	let offsetY = (window.scrollY / window.innerHeight * 10) - 5;
+
+	for(var i = 0; i < parallaxElementsNumber; i++){
+		parallaxElements[i].style.top = document.documentElement.scrollTop;
+	}
+}
